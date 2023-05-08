cabe2e6107682f24edac65a61d2763754188400b
Piyush Katole <piyushkatole12345@gmail.com>
2023-05-08 10:32:25 +0530

    udemy code

--- src/components/Helpers/Wrapper.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-const Wrapper = props =>{
-    return props.children;
-};
-
-
-export default Wrapper;
--- src/components/UI/Button.js
+++ src/components/UI/Button.js
@@ -1,19 +1,18 @@
-import React from "react";
-
+import React from 'react';
 
 import classes from './Button.module.css';
 
-const Button = props => {
-    return (
-        <button 
-            className={classes.button} 
-            type={props.type  || 'button'} 
-            onClick={props.onClick}
-        >
-            {props.children}
-        </button>
-    
-     );
+const Button = (props) => {
+  return (
+    <button
+      type={props.type || 'button'}
+      className={`${classes.button} ${props.className}`}
+      onClick={props.onClick}
+      disabled={props.disabled}
+    >
+      {props.children}
+    </button>
+  );
 };
 
 export default Button;
